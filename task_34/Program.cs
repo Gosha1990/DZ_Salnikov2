fa763610431f3f2f38a6535c731a3d2846fd9a06
Gosha1990 <123661130+Gosha1990@users.noreply.github.com>
2023-02-26 11:32:57 +0300

    task34

--- task_34/Program.cs
+++ task_34/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int size = 5;
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(100, 999);
+if (array[i] % 2 == 0)
+{
+count = count + 1;
+}
+
+}
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+Console.WriteLine($"Количество четных: {count}");
